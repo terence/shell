80a2e692ed39cc5aa5e45ad85fc5db71b0914f12
Terence <terence.chia@harro.com>
2020-04-13 09:35:52 +1000

    Update cx.sh

--- cx.sh
+++ cx.sh
@@ -7,3 +7,11 @@
 # Redhat deploy
 
 
+# Step #1 : 
+read -p "Are you sure? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+  echo "Yes selected"
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
