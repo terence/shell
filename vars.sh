36277af5c1b4289ab04a87c12c465284c64c4ce5
Terence <terence.chia@harro.com>
2021-01-21 17:02:31 +1100

    Create vars.sh

---
+++ vars.sh
@@ -0,0 +1,15 @@
+# Vars supporting shell cx.sh script                                                                   
+ 
+# Set System Commands
+APP_NOW=$(date +"%Y%m%d")
+APP_GZIP=/bin/gzip
+APP_LOCAL_TAR=/usr/bin/tar
+APP_REMOTE_TAR=/bin/tar
+APP_LN=/bin/ln
+APP_SSH=/usr/bin/ssh
+APP_SCP=/usr/bin/scp
+APP_MYSQL=/usr/bin/mysql
+APP_LOCAL_MYSQLDUMP=/Applications/MAMP/Library/bin/mysqldump
+APP_REMOTE_MYSQLDUMP=/usr/bin/mysqldump
+APP_FIND=/usr/bin/find
+APP_READ=/usr/bin/read
